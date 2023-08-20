a370030a626ce7a84963e45cd1161d348bca8bf5
veyW <veyW@users.noreply.github.com>
2023-08-20 10:56:06 -0600

    upd3

--- _cite/cite.py
+++ _cite/cite.py
@@ -27,7 +27,7 @@ log("Compiling sources")
 sources = []
 
 # in-order list of plugins to run
-plugins = ["google-scholar"]
+plugins = ["google-scholar", "pubmed"]
 
 # loop through plugins
 for plugin in plugins:
